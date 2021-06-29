581e6a7b2266652a132caf48a54dbc3235f7f1f7
Leandro Castro <leandrocastrold@hotmail.com>
2021-06-29 20:05:20 -0300

    Added database initial data

---
+++ src/main/resources/META-INF/database/database-initial-data.sql
@@ -0,0 +1,11 @@
+INSERT INTO sffighters (name, country, gender) VALUES ("Ken", "EUA", "M");
+INSERT INTO sffighters (name, country, gender) VALUES ("Ryu", "Japan", "M");
+INSERT INTO sffighters (name, country, gender) VALUES ("E. Honda", "Japan", "M");
+INSERT INTO sffighters (name, country, gender) VALUES ("Zangief", "Russia", "M");
+INSERT INTO sffighters (name, country, gender) VALUES ("Dhalsim", "India", "M");
+INSERT INTO sffighters (name, country, gender) VALUES ("Blanka", "Brazil", "M");
+
+INSERT INTO sffighters (name, country, gender) VALUES ("Laura", "Brazil", "F");
+INSERT INTO sffighters (name, country, gender) VALUES ("Elena", "Kenya", "F");
+INSERT INTO sffighters (name, country, gender) VALUES ("Cammy", "EUA", "F");
+INSERT INTO sffighters (name, country, gender) VALUES ("Chun-li", "China", "F");
